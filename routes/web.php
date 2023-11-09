afc8fc6dfb470eb60f849ba1f16ffdbb6b7d342a
takumi.oda <oda@mobilesupport.co.jp>
2023-11-09 03:58:00 +0000

    user page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController; // 追記
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +24,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+/* Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});  */
 
 require __DIR__.'/auth.php';
+
+Route::group(['middleware' => ['auth']], function () {                                    // 追記
+    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 追記
+});                                                                                       // 追記
